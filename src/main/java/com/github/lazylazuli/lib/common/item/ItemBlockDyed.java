617861659593a3c32dd714cbf9dc0db98b00423c
LazyLazuli <phoebeang@icloud.com>
2017-06-15 01:19:07 +0800

    fix dyed item stack display name

--- src/main/java/com/github/lazylazuli/lib/common/item/ItemBlockDyed.java
+++ src/main/java/com/github/lazylazuli/lib/common/item/ItemBlockDyed.java
@@ -2,17 +2,11 @@ package com.github.lazylazuli.lib.common.item;
 
 import com.github.lazylazuli.lib.common.block.BlockDyed;
 import mcp.MethodsReturnNonnullByDefault;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.translation.I18n;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
-import java.util.List;
 
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
@@ -38,11 +32,9 @@ public class ItemBlockDyed extends ItemBlockBase
 	}
 	
 	@Override
-	@SideOnly(Side.CLIENT)
-	public void addInformation(ItemStack stack, @Nullable World playerIn, List<String> tooltip, ITooltipFlag advanced)
+	public String getItemStackDisplayName(ItemStack stack)
 	{
-		tooltip.add("Color: " + I18n.translateToLocal(getUnlocalizedColor(stack)));
-		super.addInformation(stack, playerIn, tooltip, advanced);
+		return I18n.translateToLocal(getUnlocalizedColor(stack)) + " " + super.getItemStackDisplayName(stack);
 	}
 	
 	public String getUnlocalizedColor(ItemStack stack)
