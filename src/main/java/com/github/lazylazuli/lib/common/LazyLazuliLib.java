92ea9317942aa3e3ce8a211f537a093f017388fb
LazyLazuli <phoebeang@icloud.com>
2017-06-15 23:44:23 +0800

    add LazyLazuliMod.getId implementation

--- src/main/java/com/github/lazylazuli/lib/common/LazyLazuliLib.java
+++ src/main/java/com/github/lazylazuli/lib/common/LazyLazuliLib.java
@@ -3,7 +3,7 @@ package com.github.lazylazuli.lib.common;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(modid = LazyLazuliLib.MODID, version = LazyLazuliLib.VERSION, acceptedMinecraftVersions = LazyLazuliLib.MCVERSION)
-public final class LazyLazuliLib
+public final class LazyLazuliLib extends LazyLazuliMod
 {
 	public static final String MODID = "lazylazulilib";
 	public static final String VERSION = "@version@";
@@ -11,4 +11,16 @@ public final class LazyLazuliLib
 	
 	@Mod.Instance
 	public static LazyLazuliLib instance;
+	
+	@Override
+	public String getId()
+	{
+		return MODID;
+	}
+	
+	@Override
+	public Proxy getProxy()
+	{
+		return null;
+	}
 }
