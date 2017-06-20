d7db1fe984560db19c2b03033fe2f7d7fdb87674
LazyLazuli <phoebeang@icloud.com>
2017-06-21 07:27:48 +0800

    Hardcode mod info

--- src/main/java/com/github/lazylazuli/lib/common/LazyLazuliLib.java
+++ src/main/java/com/github/lazylazuli/lib/common/LazyLazuliLib.java
@@ -2,12 +2,14 @@ package com.github.lazylazuli.lib.common;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = LazyLazuliLib.MODID, version = LazyLazuliLib.VERSION, acceptedMinecraftVersions = LazyLazuliLib.MCVERSION)
+@Mod(modid = LazyLazuliLib.MODID, version = LazyLazuliLib.VERSION + "-" + LazyLazuliLib.BUILD,
+		acceptedMinecraftVersions = LazyLazuliLib.MCVERSION)
 public final class LazyLazuliLib extends LazyLazuliMod
 {
 	public static final String MODID = "lazylazulilib";
-	public static final String VERSION = "@version@";
-	public static final String MCVERSION = "@mcversion@";
+	public static final String VERSION = "2.0.0";
+	public static final String BUILD = "58";
+	public static final String MCVERSION = "1.12";
 	
 	@Mod.Instance
 	public static LazyLazuliLib instance;
