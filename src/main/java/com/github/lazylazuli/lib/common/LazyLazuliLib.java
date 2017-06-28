49d4e3ca5fcf524a9e3c69c3d3a69d2a0744b262
Rain Ang <rainnier.ang@gmail.com>
2017-06-28 21:04:44 +0800

    Refactor

--- src/main/java/com/github/lazylazuli/lib/common/LazyLazuliLib.java
+++ src/main/java/com/github/lazylazuli/lib/common/LazyLazuliLib.java
@@ -2,17 +2,19 @@ package com.github.lazylazuli.lib.common;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = LazyLazuliLib.MODID, version = LazyLazuliLib.VERSION + "-" + LazyLazuliLib.BUILD,
-		acceptedMinecraftVersions = LazyLazuliLib.MCVERSION)
+@Mod(modid = LazyLazuliLib.MODID,
+	 version = LazyLazuliLib.VERSION + "-" + LazyLazuliLib.BUILD,
+	 acceptedMinecraftVersions = LazyLazuliLib.MCVERSION)
 public final class LazyLazuliLib extends LazyLazuliMod
 {
 	public static final String MODID = "lazylazulilib";
-	public static final String VERSION = "2.0.0";
-	public static final String BUILD = "65";
+	
 	public static final String MCVERSION = "1.12";
 	
-	@Mod.Instance
-	public static LazyLazuliLib instance;
+	public static final String VERSION = "2.0.0";
+	public static final String BUILD = "65";
+	
+	@Mod.Instance public static LazyLazuliLib instance;
 	
 	@Override
 	public String getId()
