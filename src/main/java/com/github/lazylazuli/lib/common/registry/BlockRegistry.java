51c926fcaeb124a18ccf4a273ab0f759ebdfc30b
Rain Ang <rainnier.ang@gmail.com>
2017-07-03 02:58:56 +0800

    Remove registry interfaces

--- src/main/java/com/github/lazylazuli/lib/common/registry/BlockRegistry.java
+++
@@ -1,8 +0,0 @@
-package com.github.lazylazuli.lib.common.registry;
-
-import net.minecraft.block.Block;
-
-public interface BlockRegistry
-{
-	Block[] getBlocksForRegistry();
-}
--- src/main/java/com/github/lazylazuli/lib/common/registry/ItemRegistry.java
+++
@@ -1,8 +0,0 @@
-package com.github.lazylazuli.lib.common.registry;
-
-import net.minecraft.item.Item;
-
-public interface ItemRegistry
-{
-	Item[] getItemsForRegistry();
-}
